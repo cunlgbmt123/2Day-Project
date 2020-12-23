1f7839cb6d326bdfaff0278e160b55a9a3ece2cc
=Obito_i_i <=tieulong.8b@gmail.com>
2020-12-24 01:00:32 +0700

    design giao diện

---
+++ app/src/main/java/com/example/a2dayteam/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.a2dayteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.Transaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        imageView.animate().alpha(0f).setDuration(0);
+
+        textView = (TextView)findViewById(R.id.txt2);
+
+        textView.animate().alpha(0f).setDuration(0);
+
+
+        imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                textView.animate().alpha(1f).setDuration(800);
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                startActivity(intent);
+            }
+        },3000);
+    }
+}
---
+++ app/src/main/java/com/example/a2dayteam/MainMenu.java
@@ -0,0 +1,14 @@
+package com.example.a2dayteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
