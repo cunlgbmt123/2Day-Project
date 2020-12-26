dc2a3b304fe25e5262c8094a271e398ec4ca7a4f
=Obito_i_i <=tieulong.8b@gmail.com>
2020-12-27 00:55:53 +0700

    1234

--- app/src/main/java/com/example/a2dayteam/MainMenu.java
+++ app/src/main/java/com/example/a2dayteam/MainMenu.java
@@ -3,12 +3,19 @@ package com.example.a2dayteam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainMenu extends AppCompatActivity {
-
+    Button btnEmail,btnPhone,btnSignup;
+    ImageView bgimg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        final Animation Zoomin = AnimationUtils.loadAnimation(this,R.anim.)
     }
 }
